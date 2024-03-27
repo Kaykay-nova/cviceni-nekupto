9027aa3a2594adad1635fc63335431c447628575
Kristýna Konečná <kristyna.konecna02@gmail.com>
2024-03-27 15:18:59 +0100

    upravena první karta

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 const product1 = {
   image: 'images/destniklobouk.jpg',
   name: 'Deštníklobouk',
-  description: 'Vynikající pokud si často zapomínáte deštník a nechcete být nikdy překvapeni nečekanou přeháňkou.',
+  description:
+    'Vynikající pokud si často zapomínáte deštník a nechcete být nikdy překvapeni nečekanou přeháňkou.',
 };
 
 const product2 = {
   image: 'images/slepickabelka.jpg',
   name: 'Slepičkabelka',
-  description: 'Nejlepší způsob jak zaujmout neotřelou módou v ulíčkách kolem Staroměstkého náměstí.',
+  description:
+    'Nejlepší způsob jak zaujmout neotřelou módou v ulíčkách kolem Staroměstkého náměstí.',
 };
 
 const product3 = {
   image: 'images/anatoplavky.jpg',
   name: 'Anatoplavky',
-  description: 'Aby muži na rozpálených plážích kromě vašeho těla obdivovali také vaše nitro.',
+  description:
+    'Aby muži na rozpálených plážích kromě vašeho těla obdivovali také vaše nitro.',
 };
+
+const firstCard = document.querySelector('#product1');
+firstCard.classList.add('border-primary');
+
+const secondBtn = document.getElementById('product2').querySelector('button');
+secondBtn.classList.remove('btn-primary');
+
+const thirdCard = document
+  .getElementById('product3')
+  .querySelector('.card-title');
+thirdCard.classList.toggle('text-center');
+
+const product1HTML = `
+  <img class="card-img-top" src="${product1.image}" alt="Card image cap">
+  <div class="card-body">
+    <h2 class="card-title">${product1.name}</h2>
+    <p class="card-text">${product1.description}</p>
+  </div>
+  <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>
+`;
+document.querySelector("#product1").innerHTML = product1HTML
